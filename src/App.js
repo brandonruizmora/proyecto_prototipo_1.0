f3adac658fa701b16d384e2befbf6aecfa09f4ee
brandon_ruizmora <brandon_ruizmor@outlook.es>
2020-12-14 16:45:03 -0600

    formatting index and app, added full height style

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="container-fluid">
-    <div className="row">
-    <div className="col-sm-5 col-md-4 col-lg-3 bg-info">
-    detalles
-    </div>
-    <div className="col-sm-7 col-md-8 col-lg-9 bg-success">
-    mapa
-    </div>
-    </div>
+      <div className="row full-height">
+        <div className="col-sm-5 col-md-4 col-lg-3 bg-info">detalles</div>
+        <div className="col-sm-7 col-md-8 col-lg-9 bg-success">mapa</div>
+      </div>
     </div>
   );
 }
