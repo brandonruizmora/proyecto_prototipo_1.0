6ccca7f863215fb5888adef254e03ec91df30065
brandon_ruizmora <brandon_ruizmor@outlook.es>
2020-12-14 17:36:34 -0600

    added order on cols depending on screen width

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="container-fluid">
       <div className="row full-height">
-        <div className="col-sm-5 col-md-4 col-lg-3 bg-info">detalles</div>
-        <div className="col-sm-7 col-md-8 col-lg-9 bg-success">mapa</div>
+        <div className="order-1 order-sm-0 col-sm-5 col-md-4 col-lg-3 bg-info">detalles</div>
+        <div className="order-0 col-sm-7 order-sm-1 col-md-8 col-lg-9 bg-success">mapa</div>
       </div>
     </div>
   );
