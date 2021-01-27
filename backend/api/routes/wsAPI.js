1f47f0145023c2fadcb076a6712a6c51951d3526
brandon_ruizmora <brandon_ruizmor@outlook.es>
2021-01-26 22:51:32 -0600

    api now works with post method

--- backend/api/routes/wsAPI.js
+++ backend/api/routes/wsAPI.js
@@ -3,16 +3,53 @@ var router = express.Router();
 let puppeteer = require("puppeteer");
 let cheerio = require("cheerio");
 
+let lugar = {
+  nombre: "",
+};
+
 router.get("/", function (req, res, next) {
   wsasync().then((v) => {
     res.send("API is working properly " + v);
   });
 });
 
+router.post("/", function (req, res) {
+  if (!req.body.nombre_lugar) {
+    respuesta = {
+      error: true,
+      codigo: 502,
+      mensaje: "El campo nombre_lugar es requerido",
+    };
+    res.send(respuesta);
+  } else {
+    if (lugar.nombre !== "") {
+      respuesta = {
+        error: true,
+        codigo: 503,
+        mensaje: "El lugar ya fue buscado previamente",
+      };
+      res.send(respuesta);
+    } else {
+      lugar = {
+        nombre: req.body.nombre_lugar,
+      };
+      wsasync(req.body.lugar).then((v) => {
+        respuesta = {
+          error: false,
+          codigo: 200,
+          mensaje: "OK",
+          respuesta: v,
+        };
+        res.send(respuesta);
+      });
+    }
+  }
+});
+
 module.exports = router;
 
-async function wsasync() {
-  const a = await wspromise("google");
+async function wsasync(text) {
+  const a = await wspromise('oxxo');
   return a;
 }
 
