8c6b5abf60a4641b3d4b2e0c053daed7d3bb12ae
brandon_ruizmora <brandon_ruizmor@outlook.es>
2021-01-26 21:31:05 -0600

    web scrapping working

--- backend/api/routes/wsAPI.js
+++ backend/api/routes/wsAPI.js
@@ -1,57 +1,71 @@
 var express = require("express");
 var router = express.Router();
-let puppeteer = require('puppeteer')
-let cheerio = require('cheerio')
+let puppeteer = require("puppeteer");
+let cheerio = require("cheerio");
 
-router.get("/", function(req, res, next) {
-    const v = ws('oxxo')
-    res.send('API is working properly' + v);
+router.get("/", function (req, res, next) {
+  wsasync().then((v) => {
+    res.send("API is working properly " + v);
+  });
 });
 
 module.exports = router;
 
-
-const ws = (word) => { 
-
-const EMAIL_SELECTOR = '#username';
-const PASSWORD_SELECTOR = '#password';
-const SUBMIT_SELECTOR = '#app__container > main > div > form > div.login__form_action_container > button';
-const LINKEDIN_LOGIN_URL = 'https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin';
-
-if (word !== undefined) {
-    (() => {
-        puppeteer.launch({ headless: false })
-            .then(async (browser) => {
-                let page = await browser.newPage()
-                page.setViewport({ width: 1366, height: 768 });
-                await page.goto(LINKEDIN_LOGIN_URL, { waitUntil: 'domcontentloaded' })
-                await page.click(EMAIL_SELECTOR)
-                await page.keyboard.type('st1522@utr.edu.mx');
-                await page.click(PASSWORD_SELECTOR);
-                await page.keyboard.type('linkepass0117');
-                await page.click(SUBMIT_SELECTOR);
-                await page.waitForNavigation();
-                await page.goto(`https://www.linkedin.com/company/${word}/about`, { waitUntil: 'domcontentloaded' })
-                    .then(() => {
-                        const content = page.content();
-                        content
-                            .then((success) => {
-                                const $ = cheerio.load(success)
-                                const textExtracted = $('.link-without-visited-state.inline-block.ember-view').text();
-                                if (textExtracted !== undefined) {
-                                    const extractedWords = textExtracted.trim().split(' ');
-                                    console.log(extractedWords[2] + ' Employees');
-                                    return extractedWords[2] + ' Employees';
-                                } else {
-                                    console.log("Unable to fetch results. Please try again!")
-                                }
-                            })
-                    })
-            })
-            .catch((err) => {
-                console.log(" CAUGHT WITH AN ERROR ", err);
-            })
-    })()
+async function wsasync() {
+  const a = await wspromise("google");
+  return a;
 }
 
+function wspromise(word) {
+  return new Promise((resolve) => {
+    const EMAIL_SELECTOR = "#username";
+    const PASSWORD_SELECTOR = "#password";
+    const SUBMIT_SELECTOR =
+      "#app__container > main > div > form > div.login__form_action_container > button";
+    const LINKEDIN_LOGIN_URL =
+      "https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin";
+
+    if (word !== undefined) {
+      (() => {
+        puppeteer
+          .launch({ headless: false })
+          .then(async (browser) => {
+            let page = await browser.newPage();
+            page.setViewport({ width: 1366, height: 768 });
+            await page.goto(LINKEDIN_LOGIN_URL, {
+              waitUntil: "domcontentloaded",
+            });
+            await page.click(EMAIL_SELECTOR);
+            await page.keyboard.type("st1522@utr.edu.mx");
+            await page.click(PASSWORD_SELECTOR);
+            await page.keyboard.type("linkepass0117");
+            await page.click(SUBMIT_SELECTOR);
+            await page.waitForNavigation();
+            await page
+              .goto(`https://www.linkedin.com/company/${word}/about`, {
+                waitUntil: "domcontentloaded",
+              })
+              .then(() => {
+                const content = page.content();
+                content.then((success) => {
+                  const $ = cheerio.load(success);
+                  const textExtracted = $(
+                    ".link-without-visited-state.inline-block.ember-view"
+                  ).text();
+                  if (textExtracted !== undefined) {
+                    const extractedWords = textExtracted.trim().split(" ");
+                    resolve(extractedWords[2] + " Employees");
+                    browser.close();
+                  } else {
+                    console.log("Unable to fetch results. Please try again!");
+                  }
+                });
+              });
+          })
+          .catch((err) => {
+            console.log(" CAUGHT WITH AN ERROR ", err);
+          });
+      })();
+    }
+  });
 }
