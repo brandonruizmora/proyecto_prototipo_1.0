cd1882d4a80a3953e4aad5c4db734f1afe6c2620
brandon_ruizmora <brandon_ruizmor@outlook.es>
2021-02-03 20:56:27 -0600

    clg results for name, job and joblocation

--- backend/api/routes/wsAPI.js
+++ backend/api/routes/wsAPI.js
@@ -7,6 +7,11 @@ let lugar = {
   nombre: "",
 };
 
+let establecimiento = {
+  nombre: "",
+  empleados: [],
+};
+
 router.get("/", function (req, res, next) {
   wsasync().then((v) => {
     res.send("API is working properly " + v);
@@ -46,10 +51,8 @@ router.post("/", function (req, res) {
   }
 });
 
-module.exports = router;
-
 async function wsasync(text) {
-  const a = await wspromise('oxxo');
+  const a = await wspromise("oxxo");
   return a;
 }
 
@@ -92,7 +95,6 @@ function wspromise(word) {
                   if (textExtracted !== undefined) {
                     const extractedWords = textExtracted.trim().split(" ");
                     resolve(extractedWords[2] + " Employees");
-                    browser.close();
                   } else {
                     console.log("Unable to fetch results. Please try again!");
                   }
@@ -106,3 +108,124 @@ function wspromise(word) {
     }
   });
 }
+
+router.post("/personas", function (req, res) {
+  if (!req.body.nombre) {
+    respuesta = {
+      error: true,
+      codigo: 502,
+      mensaje: "El campo nombre es requerido",
+    };
+    res.send(respuesta);
+  } else {
+    if (establecimiento.nombre !== "") {
+      respuesta = {
+        error: true,
+        codigo: 503,
+        mensaje: "El lugar ya fue buscado previamente",
+      };
+      res.send(respuesta);
+    } else {
+      establecimiento = {
+        nombre: req.body.nombre,
+      };
+      wsasyncp(req.body.lugar).then((v) => {
+        respuesta = {
+          error: false,
+          codigo: 200,
+          mensaje: "OK",
+          respuesta: v,
+        };
+        res.send(respuesta);
+      });
+    }
+  }
+});
+
+async function wsasyncp(text) {
+  const a = await wspromisep("oxxo");
+  return a;
+}
+
+function wspromisep(word) {
+  return new Promise((resolve) => {
+    const EMAIL_SELECTOR = "#username";
+    const PASSWORD_SELECTOR = "#password";
+    const SUBMIT_SELECTOR =
+      "#app__container > main > div > form > div.login__form_action_container > button";
+    const LINKEDIN_LOGIN_URL =
+      "https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin";
+
+    if (word !== undefined) {
+      (() => {
+        puppeteer
+          .launch({ headless: false })
+          .then(async (browser) => {
+            let page = await browser.newPage();
+            page.setViewport({ width: 1366, height: 768 });
+            await page.goto(LINKEDIN_LOGIN_URL, {
+              waitUntil: "domcontentloaded",
+            });
+            await page.click(EMAIL_SELECTOR);
+            await page.keyboard.type("st1522@utr.edu.mx");
+            await page.click(PASSWORD_SELECTOR);
+            await page.keyboard.type("linkepass0117");
+            await page.click(SUBMIT_SELECTOR);
+            await page.waitForNavigation();
+            await page
+              .goto(
+                `https://www.linkedin.com/search/results/people/?keywords=${word}`,
+                {
+                  waitUntil: "domcontentloaded",
+                }
+              )
+              .then(() => {
+                const content = page.content();
+                content.then((success) => {
+                  const $ = cheerio.load(success);
+                  // const persona = $('.entity-result__content.entity-result__divider.pt3.pb3.t-12.t-black--light').find('a').find('span').find('span');
+                  // console.log(persona.html());
+
+                  // const thirdPersona = $('.entity-result__content.entity-result__divider.pt3.pb3.t-12.t-black--light').next();
+                  // console.log(thirdPersona.html());
+
+                  // const container = $('.entity-result__content.entity-result__divider.pt3.pb3.t-12.t-black--light .mb1 .linked-area.cursor-pointer');
+                  // console.log(container.html());
+
+                  // const persona = $('div.linked-area.cursor-pointer').find('a').find('span').find('span').each((index, element) => {
+                  //   console.log(index, $(element).text())
+                  // });
+                  // console.log(persona.html());
+
+                  const personas = $('div.entity-result div.entity-result__content div.mb1 div.linked-area').each((index, el) => {
+                    const nombre = $(el).find('a.app-aware-link').find('span[aria-hidden="true"]');
+                    const empleo = $(el).find('div.entity-result__primary-subtitle.t-14.t-black');
+                    const recide = $(el).find('div.entity-result__secondary-subtitle.t-14')
+                    // console.log(index, nombre)
+                    
+                    console.log(index, $(nombre).text(), $(empleo).text().trim(), $(recide).text().trim())
+                  });
+                  // console.log(personas.html())
+
+                  
+                  const textExtracted = $(
+                    ".reusable-search__entity-results-list.list-style-none"
+                  ).text();
+                  if (textExtracted !== undefined) {
+                    const extractedWords = textExtracted.trim().split(" ");
+                    resolve(extractedWords + " Employees");
+                  } else {
+                    console.log("Unable to fetch results. Please try again!");
+                  }
+                });
+              });
+          })
+          .catch((err) => {
+            console.log(" CAUGHT WITH AN ERROR ", err);
+          });
+      })();
+    }
+  });
+}
+
+module.exports = router;
